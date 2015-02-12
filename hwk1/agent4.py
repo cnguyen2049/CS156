b717879ed47329680396def40b0c86ccddf6f3aa
Chris <python490@gmail.com>
2015-02-12 11:58:57 -0800

    Still has some minor fixes that are needed to be done

--- hwk1/agent4.py
+++ hwk1/agent4.py
@@ -9,8 +9,8 @@ global fuel
 fuel = (int)(sys.argv[3])
 
 global letters 
-letters ={'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'P':0}
-numbers ={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I'}
+letters ={'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9}
+numbers ={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',0:'H'}
 airport = {'P':'P'}
 global airplane
 airplane = ['A','B','C','D','E','F','G','H','I']
@@ -38,7 +38,7 @@ class PriorityQueue:
 Beginning to write function to read in the text file
 """
 
-def readFile(inputFile):
+def readfile(inputFile):
 	inputMap = open(inputFile,'r')
 	startingMap = []
 	for line in inputMap:
@@ -68,17 +68,6 @@ def checkneighbors(Map,x,y):
 	else:
 		return True
 
-def is_letter(Map,letter,x,y):
-	letter = Map[x][y]
-	loopval = 0
-	for key in airplane:
-		if letter == key:
-			return True
-	return False
-def ifGoal(letter):
-	if(letter == 'P'):
-		return True
-	return False
 
 def printOut(startingMap):
 	output = ""
@@ -99,6 +88,8 @@ def Finder(startingMap,item):
 		for column in eachRow:
 			for key in item:
 				if column == key: 
+					replace = item[key]
+					startingMap[y][x] = str(replace)
 					return y,x
 			x = x + 1
 			#print "X is %d" %(x)
@@ -113,30 +104,11 @@ def euclidian(node,goal):
 	euclidian = math.sqrt(x1*x2 + y1 * y2)
 	return euclidian
 
-def manhattan(airplane,airport):
-	x = airplane.x - airport.x
-	y = airplane.y - airport.y
+def manhattan(tup1,tup2):
+	col = tup1[1] - tup2[1]
+	row = tup[0] - tup2[0]
 	manhattan = abs(x) + abs(y)
 	return manhattan
-
-
-def checkInOpen(node,succesor,f):
-	nx = node.x
-	ny = node.y
-	sx = succesor.y
-	sy	= succesor.y
-	#print nx,sx
-	#print ny, sy
-	if(nx == sx and ny == sy):
-		return True
-	
-def checkInClosed(node,succesor,f):
-	nx = node.x
-	ny = node.y
-	sx = succesor.y
-	sy	= succesor.y
-	if(nx == sx and ny == sy):
-		return True
 
 
 h = "euclidian"
@@ -149,11 +121,7 @@ else:
 """
 Beginning to implement A star algorithm
 """
-"""
-		if not is_letter(Map,letters,x,y):
-			tank = tank - int(Map[x][y])
-		if(tank < 0):
-			return None"""
+
 def astar (weathermap,start,goal): #start and goal will be Nodes
 	frontier = PriorityQueue()
 	frontier.put(0,start)
@@ -163,27 +131,62 @@ def astar (weathermap,start,goal): #start and goal will be Nodes
 	cost_so_far[start] = 0
 	while not frontier.empty():
 		current = frontier.get()
-		print current
 		if current == goal:
 			print "Goal Reached"
+			#print current
 			break
 		for next in weathermap.neighbors(current):
-			print "Next is "
-			print next
 			new_cost = cost_so_far[current] + 1
 			if next not in cost_so_far or new_cost < cost_so_far[next]:
 				cost_so_far[next] = new_cost
-				f = new_cost + heuristic(goal,next)
-				print f,next
+				f = new_cost + heuristic(next,goal)
 				frontier.put(f,next)
 				came_from[next] = current
+		#if frontier == []:
+			#return None
 	return came_from
-	
-def printStep(Map,path):
-	if(path == None):
-		print "No Solutions"
-		return None
 
+def check_fuel(Map,path):
+	length = len(path)-1
+	i = 0
+	tank = fuel
+	while i!=length:
+		tup = path[i]
+		col = tup[1]
+		row = tup[0]
+		#print col,row
+		cost = int(Map[row][col])
+		tank = tank - cost
+		i += 1
+	if tank < 0:
+		return True
+	else:
+		return False
+
+def printStep(Map,path):
+	tank = fuel
+	if(path == None):
+		print "Breaks"
+		return "No Solutions"
+	elif check_fuel(Map,path):
+		print "Breaks"
+		return "No Solution" 	
+	else:
+		i = 0
+		steps = 1
+		length = len(path)
+		while i < length:
+			print "step %d fuel is %d" %(steps,tank)
+			row = path[i][0]
+			col = path[i][1]
+			tank = tank - int(Map[row][col])
+			val = int(Map[row][col])
+			Map[row][col] = str(numbers[val])
+			print printOut(Map)			
+			#print Map
+			Map[row][col] = str(val)
+			i += 1	
+	return ""
 
 def reconstruct_path(came_from, start, goal):
 	current = goal
@@ -191,14 +194,11 @@ def reconstruct_path(came_from, start, goal):
 	while current != start:
 		current = came_from[current]
 		path.append(current)	
-	path.reverse()
 	return path	
 
 def build_edges(Map,tup):
 	x = tup[0]
 	y = tup[1]
-	print "X is %d" %x
-	print "Y is %d" %y
 	nodes = []
 	if checkneighbors(Map,x+1,y):
 		nodes.append((x+1,y))
@@ -208,85 +208,33 @@ def build_edges(Map,tup):
 		nodes.append((x,y-1))
 	if checkneighbors(Map,x,y+1):
 		nodes.append((x,y+1))
-	print nodes
 	return nodes
 def build_graph(Map,g):
 	length =len(Map[0])-1
 	height = len(Map)
-	print height
-	print length
 	y = 0	
 	for y in range(0,height):
-		#print "Y is %d" %y 
 		x = 0
 		for x in range (0,length):
-			#print "X is %d" %x
 			neighbors = build_edges(Map,(x,y))
 			g.edges.update({(y,x):neighbors})
 			x += 1
 		y+= 1
-	return g			
-Map = readFile(inputFile)
+	return g		
+
+	
+global Map 
+Map= readfile(inputFile)
 output = printOut(Map)
-#print output
-goal = Finder(Map,airport)
-print goal
-start = Finder(Map,airplane)
-#print goal
-#print start
-#path = astar(Map,start,goal)
+global goal
+goal = Finder(Map,letters)
+start = Finder(Map,airport)
 g = Graph()
-#g.edges = {(1,0):[(0,0)]}
-#g.edges.update({(2,0):[(1,1)]})
+
 g = build_graph(Map,g)
-print sorted(g.edges)
-#values = g.neighbors((0,0))
-#print values
+
 solution = astar(g,start,goal)
 s = reconstruct_path(solution,start,goal)
-print s
+#print Map
+print printStep(Map,s)
 
-"""
-g.edges = {
-    'A': ['B'],
-    'B': ['A', 'C', 'D'],
-    'C': ['A'],
-    'D': ['E', 'A'],
-    'E': ['B']
-}
-"""
-#g = build_graph(Map,g)
-#print g.edges
-#g = build_edges(Map,g,start)
-#print g.edges
-"""
-g.edges = {(0,0):[(0,1),(1,0)]} 
-#print g.edges
-
-practice = []
-practice.append((1,0))
-practice.append((0,1))
-#print practice
-g.edges = {start:practice}
-"""
-#print g.edges
-#print path[0]
-#if (2,1) == (2,1):
-	#print "true"
-#solution = reconstruct_path(path,start,goal)
-"""	
-union = set()
-union |= {(2,1)}
-union |= {(0,0)}
-union |= {(1,1)}
-union |= {(2,2)}
-print union
-
-testq = PriorityQueue()
-
-tuple2 = (2,1)
-testq.put(0,tuple2)
-current = testq.get()
-x = current[0]
-print x
-"""
