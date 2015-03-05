249bc307a59a0e0246044bd457c84b9302c00568
python490 <python490@gmail.com>
2015-03-04 22:23:20 -0800

    Final version of everything

--- hwk2/connect_earthquake.py
+++ hwk2/connect_earthquake.py
@@ -1,6 +1,9 @@
 """
-This module was designed to handle th ecommand line arguments for the 
+This module was designed to handle th ecommand line arguments for the
 Connect Earthquake assignment implementing a Hill Climbing algorithm.
+- Chris Nguyen
+- Matthew Carlis
+- Shunt Balushian
 """
 import sys
 
@@ -9,6 +12,7 @@ from quake_connect import ConnectFour
 CRAP_OUT = '\nERROR with human input.'
 CRAP_OUT += '\nYou entered something wrong.  Stop being a Type Noob.'
 
+
 def run_simulation(player_first=False, indicate_quake=False):
     try:
         game_obj = ConnectFour(player_first, indicate_quake)
@@ -16,14 +20,21 @@ def run_simulation(player_first=False, indicate_quake=False):
     except ValueError:
         print CRAP_OUT
         sys.exit(1)
-        
+    except KeyboardInterrupt:
+        print '\n\n          I win by your submission human!'
+        print 'Maybe you need larger heat sinks to handle this heat\n'
+        sys.exit(1)
+
 
 def parse_arguments(arguments):
     prompt_indicate = 'Should I tell you when earthquake happen (y/n)?\n'
     prompt_indicate += '(If not, each round, I will ask you if an earthquake happened): '
     try:
-        player_first = raw_input('Would you like to go first (y/n)?: ').strip(' ') == 'y'
+        player_first = raw_input(
+            'Would you like to go first (y/n)?: ').strip(' ') == 'y'
         indicate_quake = raw_input(prompt_indicate).strip(' ') == 'y'
+    except KeyboardInterrupt:
+        sys.exit(1)
     except Exception:
         print CRAP_OUT
         sys.exit(1)
@@ -36,4 +47,3 @@ if __name__ == '__main__':
     ARGS = copy.deepcopy(sys.argv)
     PLAYER, QUAKE = parse_arguments(ARGS)
     run_simulation(PLAYER, QUAKE)
-
